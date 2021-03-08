f3b6401dd971f66d6b63625c7490f68f81df0925
dviedma <dviedm@gmail.com>
2021-03-08 13:22:44 -0500

    done

--- components/shell/Footer.js
+++ components/shell/Footer.js
@@ -6,22 +6,18 @@ const Footer = () => {
 
   return (
     <footer className="container-fluid pt-5 pb-2">
-      {/*
       <div className="row justify-content-sm-center">
         <div className="col-sm-3">
           <ul>
-            <li className="text-center"><Link href="/about"><a>About Us</a></Link></li>
-            <li className="text-center"><Link href="/contact"><a>Contact</a></Link></li>
+            <li className="text-center"><a href="mailto:admin@vrg.live" target="_blank">Contact</a></li>
           </ul>          
         </div>    
         <div className="col-sm-3">
           <ul>
-            <li className="text-center"><a href="https://instagram.com" target="_blank">Instagram</a></li>
-            <li className="text-center"><a href="https://facebook.com" target="_blank">Facebook</a></li>
+            <li className="text-center"><a href="https://instagram.com/vrg.live/" target="_blank">Instagram</a></li>
           </ul>
         </div>  
       </div>
-       */}
       <div className="row justify-content-md-center mt-3">
         <div className="col">
           <p className="small text-center">© VRG 2021</p>
